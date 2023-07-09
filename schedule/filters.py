0de95df23760bdb4dfc8b3a9a79bd979f29bd660
Paweł Kordowski <paw.mar.kor@gmail.com>
2023-07-09 23:47:02 +0200

    Implement filtering by today

--- schedule/filters.py
+++ schedule/filters.py
@@ -1,5 +1,13 @@
-from django_filters.rest_framework import FilterSet, CharFilter
+from django.utils import timezone
+from django_filters.rest_framework import FilterSet, CharFilter, BooleanFilter
 
 
 class ScheduleFilter(FilterSet):
     class_name = CharFilter("klass__name")
+    for_today = BooleanFilter(method="for_today_filter")
+
+    def for_today_filter(self, queryset, name, value):
+        if value:
+            return queryset.filter(day_of_week=timezone.now().strftime("%w"))
+
+        return queryset
--- schedule/tests.py
+++ schedule/tests.py
@@ -1,4 +1,5 @@
 import pytest
+from django.utils import timezone
 from rest_framework.status import HTTP_200_OK
 
 from schedule.factories import ScheduleFactory, StudentFactory
@@ -13,8 +14,10 @@ def test_get_schedule_response_200(client):
 @pytest.mark.django_db
 def test_get_schedule_returns_all_items(client):
     ScheduleFactory.create_batch(size=3)
+
     response = client.get("/schedule/")
     data = response.json()
+
     assert len(data) == 3
 
 
@@ -25,8 +28,10 @@ def test_get_schedule_ordered_by_day_of_week_and_hours(client):
     ScheduleFactory(day_of_week=0, hour=0)
     ScheduleFactory(day_of_week=2, hour=0)
     ScheduleFactory(day_of_week=1, hour=2)
+
     response = client.get("/schedule/")
     data = response.json()
+
     assert [(d["day_of_week"], d["hour"]) for d in data] == [
         (0, 0),
         (1, 0),
@@ -43,9 +48,11 @@ def test_get_schedule_payload(client, django_assert_num_queries):
     schedule_2 = ScheduleFactory(klass__name="5B", day_of_week=1)
     StudentFactory.create_batch(size=5, klass=schedule_2.klass)
     StudentFactory()
+
     with django_assert_num_queries(2):
         response = client.get("/schedule/")
     data = response.json()
+
     assert data == [
         {
             "class": {"name": "5A", "student_count": 3},
@@ -68,7 +75,26 @@ def test_get_schedule_payload(client, django_assert_num_queries):
 def test_get_schedule_filter_by_class_name(client):
     ScheduleFactory(klass__name="5A")
     ScheduleFactory()
+
     response = client.get("/schedule/", data={"class_name": "5A"})
     data = response.json()
+
     assert len(data) == 1
     assert data[0]["class"]["name"] == "5A"
+
+
+@pytest.mark.django_db
+def test_get_schedule_filter_by_today(client):
+    for day_of_week in range(7):
+        ScheduleFactory(day_of_week=day_of_week)
+
+    response = client.get("/schedule/", data={"for_today": True})
+    data = response.json()
+
+    assert len(data) == 1
+    assert data[0]["day_of_week"] == int(timezone.now().strftime("%w"))
+
+    response = client.get("/schedule/", data={"for_today": False})
+    data = response.json()
+
+    assert len(data) == 7
